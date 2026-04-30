e1d242b8ba1bfd37f0c65ec26555ff91c8c9957a
Amir Reza Azemati <33527043+amirreza98@users.noreply.github.com>
2026-04-30 10:10:15 +0200

    Refactor functions and demonstrate usage in main

--- 05_functions/example.dart
+++ 05_functions/example.dart
@@ -1,18 +1,34 @@
+//Basic Function Syntax:
+returnType functionName(parameters) {
+  // logic
+  return value;
+}
+//Or short version:
+returnType functionName(parameters) => expression;
+
+// Entry Point of every Dart program:
 void main() {
+  //invoke:
   greet('Sara');
+  //${...} is string interpolation
   print('10 + 25 = ${add(10, 25)}');
   print(getGrade(87));
   print(isAdult(16));
 
+  //Anonymous fun
   var multiply = (int a, int b) => a * b;
   print(multiply(4, 5));
 
   List<int> scores = [55, 88, 42, 91, 73];
+
+  //Functions Inside Collections
   print(scores.where((s) => s >= 60).toList());
 }
 
+//defenition:
 void greet(String name) => print('Hello, $name!');
 
+//arrow syntax (=>) → shorthand for one-line functions:
 int add(int a, int b) => a + b;
 
 String getGrade(int score) {
@@ -23,4 +39,5 @@ String getGrade(int score) {
   return 'F';
 }
 
+//truly or falsy
 bool isAdult(int age) => age >= 18;
