e2a83f2f24d42003a2482873def143c7b1032e45
Amir Reza Azemati <33527043+amirreza98@users.noreply.github.com>
2026-04-30 11:06:11 +0200

    Enhance Student class with constructor and methods
    Added constructor and methods to Student class.

--- 06_classes/example.dart
+++ 06_classes/example.dart
@@ -1,4 +1,16 @@
+//OOP language
+//blueprint (template) for creating objects
+class Student {
+  String name;
+  int age;
+  List<int> scores;
+  ...
+}
+//Class = design
+//Object = actual instance
+
 void main() {
+  //Creating Objects (Instances)
   Student s1 = Student('Sara Mueller', 22, [88, 72, 95, 61]);
   Student s2 = Student('Max Schneider', 19, [55, 61, 70, 58]);
 
@@ -9,17 +21,29 @@ void main() {
 
   List<Student> classroom = [s1, s2];
   for (Student s in classroom) {
+    //treating objects like any other data type:
     print('${s.name} — ${s.getStatus()}');
   }
 }
 
 class Student {
+  //Fields (Properties):
+  //meaning Every Student object will have these pieces of data.
   String name;
   int age;
   List<int> scores;
 
+  //Constructor:
+  //meaning when someone creates a Student, this is how we assign values to those fields
+  Student(String name, int age, List<int> scores) {
+    this.name = name;
+    this.age = age;
+    this.scores = scores;
+  }
+  //or shorthand constructor:
   Student(this.name, this.age, this.scores);
 
+  //Methods:
   void introduce() => print('Hi, I am $name, $age years old.');
 
   double getAverage() {
